d818f183f7113044d0a62b67913d5ad27051fa97
Matias <mati.delarosa1984@gmail.com>
2024-01-22 01:09:47 -0300

    functionality signup page

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,8 @@ export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
-      <Link className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+      <Link to='/'
+      className='font-bold dark:text-white text-4xl'>
       <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg'>Matias</span>
       Blog
       </Link>
